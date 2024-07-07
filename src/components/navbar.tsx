e8896a3241ebaecd823ee57ebc8e6f5996191743
varawa <142249631+varawa@users.noreply.github.com>
2024-07-07 17:52:13 +0530

    navBar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,27 @@ function Navbar({ className }: { className?: string }){
     const [active , setActive] = useState<string | null>(null) ;
     return(
         <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-            NavBar
+            <Menu setActive={setActive}>
+                <Link href={"/"}>
+                        <MenuItem setActive={setActive} active={active} item="Home">
+                        </MenuItem>
+                </Link>
+                <Link href={"/"}>
+                        <MenuItem setActive={setActive} active={active} item="Our Courses">
+                            <div className="flex flex-col space-y-4 text-sm">
+                                <HoveredLink href="/courses">All Courses</HoveredLink>
+                                <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+                                <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+                                <HoveredLink href="/courses">SongWriting</HoveredLink>
+                                <HoveredLink href="/courses">Music Production</HoveredLink>
+                            </div>
+                        </MenuItem>
+                </Link>
+                <Link href={"/contact"}>
+                        <MenuItem setActive={setActive} active={active} item="Contact Us">
+                        </MenuItem>
+                </Link>
+            </Menu>
         </div>
     )
 }
